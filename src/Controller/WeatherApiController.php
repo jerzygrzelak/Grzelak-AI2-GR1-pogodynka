1b97f3cacb155da5a7ca43fa01feed98b6e73b69
jerzygrzelak <greenbanzai@gmail.com>
2021-11-25 22:11:40 +0100

    lab7 getting the request data

--- src/Controller/WeatherApiController.php
+++ src/Controller/WeatherApiController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\CityRepository;
+use App\Repository\MeasurementRepository;
 use App\Service\WeatherUtil;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class WeatherApiController extends AbstractController
 {
@@ -17,8 +20,20 @@ class WeatherApiController extends AbstractController
         ]);
     }
 
-    public function weatherJsonAction(string $country,string $city, string $format, WeatherUtil $weatherUtil):Response{
+    public function weatherJsonAction(WeatherUtil $weatherUtil, CityRepository $cityRepository,
+                                      MeasurementRepository $measurementRepository, Request $request, SerializerInterface $serializer):Response{
+        $payload=$request->getContent();
+        $payload=json_decode($payload,true);
+        $country=$payload['country'];
+        $city=$payload['city'];
         $response=new Response();
+        $measurements=$weatherUtil->getWeatherForCountryAndCity($country,$city,$measurementRepository, $cityRepository);
+        $json=$serializer->serialize($measurements['measurements'],'json');
+        dd($json);
+//        $response->setContent(json_encode($measurements));
+//        $response->headers->set('Content-Type','application/json');
+//        $response->setContent(json_encode($measurements['city']->getName()));
+//        $response->setContent(json_encode($measurements['city']->getLatitude()));
         return $response;
     }
 }
